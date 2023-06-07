2124a6763ae830992002160c766f6cf6da0829df
Gabriel Xavier Pereira <gabriel220060@ilum.cnpem.br>
2023-06-07 19:56:48 -0300

    Criei o arquivo de funções

---
+++ .ipynb_checkpoints/funcoes-checkpoint.py
@@ -0,0 +1,19 @@
+def funcao_extrair_features_cif(dataframe):
+"""Essa função gera, a partir dos dados de cif de cada material, novas colunas contendo as propriedades contidas nas cifs: cada átomo do material e cada posição.
+
+    Args:
+        dataframe: conjunto de dados do problema
+
+    Return:
+        Um novo dataframe contendo novas colunas relativas aos dados da cif
+"""
+
+def funcao_extrair_features_isotherms(dataframe):
+"""Essa função gera, a partir dos dados de isotherm de cada material, novas colunas contendo as propriedades contidas nos isotherms: adsorção de gases em pressões variadas.
+
+    Args:
+        dataframe: conjunto de dados do problema
+
+    Return:
+        Um novo dataframe contendo novas colunas relativas aos dados de isotherm
+"""
